441b5d8c6507db09ed9ba2c30caa15cb95bff52c
栗山玲依 <kuriyamareii@kuriyamareii-no-MacBook-Pro.local>
2018-08-14 17:45:26 +0900

    つらい

--- practice2.js
+++ practice2.js
@@ -1,3 +1,37 @@
-for (let i =0; i< 10; i++){
-    console.log(i);
+let mes = '';
+
+for (let i =1; i< 10; i++){
+    for (let j=1; j< 10; j++){
+        // console.log(i);
+        mes = mes + ' ' + i*j;
+    }
 }
+
+// let mes = 'あいうえお';
+console.log(mes);
+
+// mes = '';
+// console.log(mes);
+
+// mes = 'mes';
+// console.log(mes);
+
+// mes = mes + 'mes';
+// console.log(mes);
+
+// mes = mes + 'mes';
+// console.log(mes);
+
+// mes = '';
+// console.log(mes);
+
+// let i = 2;
+// let j = 8;
+// mes = i*j;
+
+// i=2;
+// j=9;
+
+// mes = mes + ' ' + i*j;
+// console.log(mes);
+
--- server4.js
+++ server4.js
@@ -1,8 +1,23 @@
-let mes = '';
-for (let i =1; i< 10; i++){
-    console.log(i);
-    for(let j =1; j< 10; j++){
-        console.log(j*i);
-    }
+'use strict';
 
-}
+const express = require('express') ;
+const app = express();
+const PORT = 3000;
+const bodyParser = require('body-parser');
+app.use(bodyParser()); //宣言
+app.use(express.static('public')); //宣言
+
+app.get('/',  (req, res)  => res.send('Hello World'));
+app.get('/kuku',  (req, res)  => {
+    let mes = '';
+    for (let i =1; i< 10; i++){
+        for(let j =1; j< 10; j++){
+            mes = mes + ' ' + i*j;
+        }
+    mes = mes + '<br>';
+    }
+    res.send(mes);
+})
+
+app.listen(PORT);
+console.log(`listening on *: ${PORT}`);
