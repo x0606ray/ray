fe4c407a1759d2b8054478b3e5b6b548f95b8ccc
栗山玲依 <kuriyamareii@kuriyamareii-no-MacBook-Pro.local>
2018-08-16 16:20:17 +0900

    コメント

---
+++ day4/mychat/server.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('chat message', (msg) => {
+        // io.emit('chat message', msg);
+        socket.broadcast.emit('chat message', msg)
+        console.log(`message: ${msg}`);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
+
+});
+
+http.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
+});
